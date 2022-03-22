5c6727f49ddf0bcabf4a2ca2c92ecd4a126ec011
erikturkovic <eturkovic@student.unipu.hr>
2022-03-22 18:15:18 +0100

    4 ig carda ispis gotov
    različiti id n stuff. info, cards... vjezba 3 gotova

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,31 @@
   <div class="row">
     <div class="col-2"></div>
     <div class="col-7">
-      <InstagramCard />
+      <InstagramCard v-for="card in cards" :key="card" :info="card" />
     </div>
-    <div class="col-3">... ovdje ide desni stupac!</div>
+    <div class="col-3"></div>
   </div>
 </template>
 
 <script>
 import InstagramCard from "@/components/InstagramCard.vue";
 
+let cards = [];
+
+cards = [
+  "https://picsum.photos/id/1/400/400",
+  "https://picsum.photos/id/2/400/400",
+  "https://picsum.photos/id/3/400/400",
+  "https://picsum.photos/id/4/400/400",
+];
+
 export default {
   name: "home",
+  data: function () {
+    return {
+      cards: cards,
+    };
+  },
   components: {
     InstagramCard,
   },
