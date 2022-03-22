1d5e360c1e9e10ad5d9f524397e8921384ce6084
erikturkovic <eturkovic@student.unipu.hr>
2022-03-22 17:47:52 +0100

    Uređivanje koda

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row">
+    <div class="col-2"></div>
+    <div class="col-7">
+      <InstagramCard />
+    </div>
+    <div class="col-3">... ovdje ide desni stupac!</div>
+  </div>
+</template>
+
+<script>
+import InstagramCard from "@/components/InstagramCard.vue";
+
+export default {
+  name: "home",
+  components: {
+    InstagramCard,
+  },
+};
+</script>
